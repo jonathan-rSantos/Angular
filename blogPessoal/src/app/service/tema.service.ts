a48b673a84ad26a62e90347635dd36bf9820f89e
Jonathan <jonathan.ribeiro28121997@gmail.com>
2021-06-04 13:52:33 -0300

    finalização de CRUD de tema

--- blogPessoal/src/app/service/tema.service.ts
+++ blogPessoal/src/app/service/tema.service.ts
@@ -18,17 +18,25 @@ getAllTema(): Observable<Tema[]>{
 return this.http.get<Tema[]>(`${environment.server}/tema`, this.token)
 }
 
-/* getByIdTema(id: number): Observable<Tema[]>{
-  return this.http.get<Tema[]>(`${environment.server}/tema/{id}`, this.token)
+getByIdTema(id: number): Observable<Tema>{
+  return this.http.get<Tema>(`${environment.server}/tema/${id}`, this.token)
   }
 
-  getByDescricaoTema(): Observable<Tema[]>{
-    return this.http.get<Tema[]>(`${environment.server}/tema/descricao/{descricao}`, this.token)
+  getByDescricaoTema(nome: string): Observable<Tema[]>{
+    return this.http.get<Tema[]>(`${environment.server}/tema/nome/${nome}`, this.token)
     }
- */
+
 postTema(tema: Tema): Observable<Tema>{
 return this.http.post<Tema>(`${environment.server}/tema`, tema, this.token)
 }
 
+
+putTema(tema: Tema): Observable<Tema>{
+  return this.http.put<Tema>(`${environment.server}/tema`,tema, this.token)
+}
+
+deleteTema(id: number){
+  return this.http.delete(`${environment.server}/tema/${id}`, this.token)
+}
 
 }
